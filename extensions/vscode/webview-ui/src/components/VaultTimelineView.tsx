39fd717518fbdc3c1bce643731c8b22cc4c6b9db
Anicet Nougaret <an.nougaret@gmail.com>
2025-06-16 11:56:47 +0200

    final timeline touches

--- extensions/vscode/webview-ui/src/components/VaultTimelineView.tsx
+++ extensions/vscode/webview-ui/src/components/VaultTimelineView.tsx
@@ -7,6 +7,7 @@ import { ConstructionTraceTree } from '../bindings/ConstructionTraceTree';
 import { getRelativePath } from '../utils/pathUtils';
 import { useWorkspaceRoots } from '../hooks/useWorkspaceRoots';
 import { Timestamp } from '../bindings/Timestamp';
+import { useTheme } from '../hooks/useTheme';
 
 function resolveTimestamp(timeline: { min_ts: number; max_ts: number; }, timestamp: Timestamp, isStart: boolean): number {
   if (timestamp === 'Unknown') {
@@ -15,11 +16,13 @@ function resolveTimestamp(timeline: { min_ts: number; max_ts: number; }, timesta
   return timestamp.Known;
 }
 
-const SpanComponent: React.FC<{ 
+const SpanComponent: React.FC<{
   timeline: Timeline;
   spanId: string;
   containerWidth: number;
-}> = ({ timeline, spanId, containerWidth }) => {
+  hoveredSpan: string | null;
+  onHover: (spanId: string) => void;
+}> = ({ timeline, spanId, containerWidth, hoveredSpan, onHover }) => {
   const span = timeline.spans[spanId];
   if (!span) {
     return null;
@@ -29,12 +32,16 @@ const SpanComponent: React.FC<{
   const endTimestamp = resolveTimestamp(timeline, span.end, false);
 
   const timelineStartPos = timeline.timestampsToTimelinePosition.get(startTimestamp) ?? -1;
-  const timelineEndPos = timeline.timestampsToTimelinePosition.get(endTimestamp) ?? -1;
-  
+  let timelineEndPos = timeline.timestampsToTimelinePosition.get(endTimestamp) ?? -1;
+
   if (timelineStartPos === -1 || timelineEndPos === -1) {
     return null;
   }
-  
+
+  if (timelineStartPos === timelineEndPos) {
+    timelineEndPos = Math.min(timelineEndPos + 1, timeline.timestampsToTimelinePosition.size - 1);
+  }
+
   const spanLeft = (timelineStartPos / timeline.timestampsToTimelinePosition.size) * containerWidth;
   const spanWidth = Math.max(
     2,
@@ -52,52 +59,90 @@ const SpanComponent: React.FC<{
   const endPosition = enterTrace.end_pos;
 
   return (
-    <div
-      key={spanId}
-      className={cn(
-        'absolute top-1/2 -translate-y-1/2 h-6 bg-[var(--info-muted)] cursor-pointer hover:bg-[var(--info-base)] hover:border-2 hover:border-[var(--bg-base)] hover:outline-2 hover:outline-[var(--info-muted)] rounded-md'
-      )}
-      style={{
-        left: `${spanLeft}px`,
-        width: `${spanWidth}px`,
-      }}
-      title={`${startPosition.filepath}:${startPosition.line}:${startPosition.column} - ${endPosition.filepath}:${endPosition.line}:${endPosition.column}. ${timelineStartPos} - ${timelineEndPos} ${startTimestamp} - ${endTimestamp}`}
-      onMouseEnter={() => {
-        requestHighlight(startPosition.filepath, startPosition.line, startPosition.column, endPosition.line, endPosition.column);
-      }}
-    />
+      <div
+        key={spanId}
+        className={cn(
+          'absolute top-1/2 -translate-y-1/2 h-16 bg-[var(--info-muted)] cursor-pointer rounded-md',
+          span.is_error && 'bg-[var(--error-muted)]',
+          hoveredSpan === spanId && (
+            span.is_error ? 'bg-[var(--error-base)] border-2 border-[var(--bg-base)] outline-2 outline-[var(--error-muted)]' : 'bg-[var(--info-base)] border-2 border-[var(--bg-base)] outline-2 outline-[var(--info-muted)]'
+          )
+        )}
+        style={{
+          left: `${spanLeft}px`,
+          width: `${spanWidth}px`,
+        }}
+        // title={`${startPosition.filepath}:${startPosition.line}:${startPosition.column} - ${endPosition.filepath}:${endPosition.line}:${endPosition.column}. ${timelineStartPos} - ${timelineEndPos} ${startTimestamp} - ${endTimestamp}`}
+        onMouseEnter={() => {
+          requestHighlight(startPosition.filepath, startPosition.line, startPosition.column, endPosition.line, endPosition.column);
+          onHover(spanId);
+        }}
+      />
   );
 };
 
-const FamilyComponent: React.FC<{ 
+const FamilyComponent: React.FC<{
   timeline: Timeline;
   familyId: string;
   containerWidth: number;
 }> = ({ timeline, familyId, containerWidth }) => {
+  const [hoveredSpan, setHoveredSpan] = useState<string | null>(null);
+
   const spans = timeline.familiesToSpans.get(familyId);
   if (!spans || spans.size === 0) {
     return null;
   }
 
   return (
-    <div className='relative h-14 w-full my-1'>
-      {spans.values().map((spanId, index) => (
-        <SpanComponent 
-          key={index} 
-          timeline={timeline} 
-          spanId={spanId} 
-          containerWidth={containerWidth} 
-        />
-      ))}
+    <div className="flex flex-col w-full"
+    onMouseEnter={() => setHoveredSpan(null)}
+    onMouseLeave={() => setHoveredSpan(null)}
+    >
+      <div 
+        className='relative min-h-20 h-20 w-full'
+      >
+        {spans.values().map((spanId, index) => (
+          <SpanComponent
+            key={index}
+            timeline={timeline}
+            spanId={spanId}
+            containerWidth={containerWidth}
+            hoveredSpan={hoveredSpan}
+            onHover={setHoveredSpan}
+          />
+        ))}
+      </div>
+      {hoveredSpan && (() => {
+        const span = timeline.spans[hoveredSpan];
+        const subTimeline = {
+          ...timeline,
+          displayedFamilies: new Set([timeline.familiesToSpans.get(span.trace_id) ? span.trace_id : null, span.indirect_children_family?.[0] || null].filter((f) => f !== null)),
+        };
+
+        if (subTimeline.displayedFamilies.size === 0) {
+          return null;
+        }
+
+        return (
+          <div className="w-full border-2 border-[var(--bg-450)] shadow-lg rounded-xl overflow-hidden">
+            <TimelineComp timeline={subTimeline} />
+          </div>
+        );
+      })()}
     </div>
   );
 };
 
-const TimelineCluster: React.FC<{ timeline: Timeline; filepath: string }> = ({ timeline, filepath }) => {
+const TimelineCluster: React.FC<{
+  timeline: Timeline;
+  filepath: string;
+  familyIds: string[];
+}> = ({ timeline, filepath, familyIds }) => {
   const [clusterExpanded, setClusterExpanded] = useState(true);
   const ref = useRef<HTMLDivElement>(null);
   const [width, setWidth] = useState<number>(0);
   const workspaceRoots = useWorkspaceRoots();
+  const { isDark } = useTheme();
 
   useEffect(() => {
     const updateWidth = () => {
@@ -112,20 +157,20 @@ const TimelineCluster: React.FC<{ timeline: Timeline; filepath: string }> = ({ t
 
   return (
     <div className="flex flex-col w-full max-w-full">
-      <button
+      {/* <button
+        disabled
         onClick={() => setClusterExpanded(!clusterExpanded)}
-        className="flex px-1.5 py-0.5 bg-[var(--bg-550)] sticky top-0 z-10"
+        className={cn(
+          "flex px-1.5 py-0.5 sticky top-0 z-10",
+          isDark ? "bg-[var(--bg-400)]" : "bg-[var(--bg-550)]"
+        )}
       >
         {getRelativePath(filepath, workspaceRoots)}
-      </button>
+      </button> */}
       {clusterExpanded && (
-        <div className="flex flex-col w-full px-3">
-          <div className="w-full h-1" ref={ref}></div>
-          {timeline.filesToFamilies.get(filepath)?.values().map((familyId, index) => {
-            const orphan = timeline.orphan_families_with_no_indirect_parent.includes(familyId);
-            if (!orphan) {
-              return null;
-            }
+        <div className="flex flex-col w-full p-3">
+          <div className="w-full h-0" ref={ref}></div>
+          {familyIds.map((familyId, index) => {
             return (
               <FamilyComponent
                 key={index}
@@ -142,18 +187,76 @@ const TimelineCluster: React.FC<{ timeline: Timeline; filepath: string }> = ({ t
 };
 
 type Timeline = ConstructionTraceTree & {
+  // global
   spansToTraces: Map<string, {
     enter: number,
     exitOrError: number
   }>;
   filesToFamilies: Map<string, Set<string>>;
   familiesToSpans: Map<string, Set<string>>;
+
+  // local
+  displayedFamilies: Set<string>;
   timestampsToTimelinePosition: Map<number, number>;
 };
 
-interface VaultTimelineViewProps {}
+function computeTimestampsToTimelinePosition(timeline: Timeline): Map<number, number> {
+  let uniqueTimestamps: Set<number> = new Set();
+  
+  timeline.displayedFamilies.forEach((familyId) => {
+    const spans = timeline.familiesToSpans.get(familyId);
+    if (spans) {
+      spans.forEach((spanId) => {
+        const span = timeline.spans[spanId];
+        if (span) {
+          const startTimestamp = resolveTimestamp(timeline, span.start, true);
+          const endTimestamp = resolveTimestamp(timeline, span.end, false);
+          uniqueTimestamps.add(startTimestamp);
+          uniqueTimestamps.add(endTimestamp);
+        }
+      });
+    }
+  });
 
-const VaultTimelineView: React.FC<VaultTimelineViewProps> = ({}) => {
+  uniqueTimestamps.add(timeline.min_ts);
+  uniqueTimestamps.add(timeline.max_ts);
+
+  const sortedTimestamps = Array.from(uniqueTimestamps).sort((a, b) => a - b);
+  const timestampsToTimelinePosition: Map<number, number> = new Map();
+  sortedTimestamps.forEach((timestamp, index) => {
+    timestampsToTimelinePosition.set(timestamp, index);
+  });
+
+  return timestampsToTimelinePosition;
+}
+
+const TimelineComp: React.FC<{ timeline: Timeline }> = ({ timeline }) => {
+  timeline.timestampsToTimelinePosition = computeTimestampsToTimelinePosition(timeline);
+  
+  return (
+    <div className="bg-[var(--bg-base)] flex flex-col w-full h-full max-w-full max-h-full text-[var(--fg-base)] overflow-auto">
+      {timeline.filesToFamilies.keys().map((filepath, index) => {
+        const topLevelFamilies = Array.from(timeline.filesToFamilies
+          .get(filepath)
+          ?.values()
+          .filter((familyId) => timeline.displayedFamilies.has(familyId)) || []);
+        
+        return (
+          <TimelineCluster
+            key={index}
+            timeline={timeline}
+            filepath={filepath}
+            familyIds={topLevelFamilies}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+interface VaultTimelineViewProps { }
+
+const VaultTimelineView: React.FC<VaultTimelineViewProps> = ({ }) => {
   const focusedVault = useFocusedVault();
   const tree = useConstructionTree();
 
@@ -196,14 +299,10 @@ const VaultTimelineView: React.FC<VaultTimelineViewProps> = ({}) => {
 
   let familiesToSpans: Map<string, Set<string>> = new Map();
   let filesToFamilies: Map<string, Set<string>> = new Map();
-  let uniqueTimestamps: Set<number> = new Set();
-  uniqueTimestamps.add(tree.min_ts);
-  uniqueTimestamps.add(tree.max_ts);
 
   spansToTraces.entries().forEach(([traceId, { enter, exitOrError }]) => {
     const trace = tree.traces.items[enter];
     const span = tree.spans[traceId];
-    const exitOrErrorTrace = tree.traces.items[exitOrError];
     if (trace && span) {
       const familyId = trace.parent_id;
       if (!familiesToSpans.has(familyId)) {
@@ -215,19 +314,7 @@ const VaultTimelineView: React.FC<VaultTimelineViewProps> = ({}) => {
         filesToFamilies.set(trace.start_pos.filepath, new Set());
       }
       filesToFamilies.get(trace.start_pos.filepath)!.add(familyId);
-
-      const startTimestamp = resolveTimestamp(tree, span.start, true);
-      const endTimestamp = resolveTimestamp(tree, span.end, false);
-
-      uniqueTimestamps.add(startTimestamp);
-      uniqueTimestamps.add(endTimestamp);
     }
-  });
-
-  const sortedTimestamps = Array.from(uniqueTimestamps).sort((a, b) => a - b);
-  const timestampsToTimelinePosition: Map<number, number> = new Map();
-  sortedTimestamps.forEach((timestamp, index) => {
-    timestampsToTimelinePosition.set(timestamp, index);
   });
 
   let timeline: Timeline = {
@@ -235,17 +322,12 @@ const VaultTimelineView: React.FC<VaultTimelineViewProps> = ({}) => {
     spansToTraces,
     filesToFamilies,
     familiesToSpans,
-    timestampsToTimelinePosition,
+    timestampsToTimelinePosition: new Map(),
+    displayedFamilies: new Set(tree.orphan_families_with_no_indirect_parent),
   };
 
-  console.log('timeline', timeline);
-
   return (
-    <div className="bg-[var(--bg-base)] flex flex-col w-full h-full max-w-full max-h-full text-[var(--fg-base)] overflow-auto">
-      {timeline.filesToFamilies.keys().map((filepath, index) =>
-        <TimelineCluster key={index} timeline={timeline} filepath={filepath} />
-      )}
-    </div>
+    <TimelineComp timeline={timeline} />
   );
 };
 
